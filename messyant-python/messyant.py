c74511bc0e8cb5a87e6d1440aacb7590ad566e4b
Carson Bloomingdale <cbloomingdale@hubspot.com>
2021-01-10 15:57:16 -0500

    add environment variable ocntext

--- messyant-python/messyant.py
+++ messyant-python/messyant.py
@@ -1,14 +1,25 @@
+import os
 from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-class Greeting (Resource):
-   def get(self):
-      return { "message" : "Hello Flask API World!" }
+def sql_conn():
+    conn = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};"
+                          "Server=;"
+                          "Database=" + os.environ['DATABASE_ENDPOINT'] + ";"
+                          "uid=" + os.environ['DATABASE_ENDPOINT_USERNAME'] +
+                          ";pwd=" + os.environ['DATABASE_ENDPOINT_PASSWORD'] + ";")
+    return conn.cursor()
 
-api.add_resource(Greeting, '/') # Route_1
+class User (Resource):
+   def get(self):
+      cursor = sql_conn()
+      cursor.execute('SELECT * FROM User')
+      return { "response" : cursor }
+
+api.add_resource(User, '/') # Route_1
 
 if __name__ == '__main__':
    app.run('0.0.0.0','8080')
